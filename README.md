9adf3af1871edf9e101a2bab073921e9de0e6015
Aditya Srivastav <adityasrivastav1355@gmail.com>
2025-07-24 23:49:15 +0530

    Okay changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-Learning
 This is a repo of my python journey from CodeWithHarry from scratch
+
--- chapter 2/02_datatype.py
+++ chapter 2/02_datatype.py
@@ -1,3 +1,5 @@
+# THis is my basic codes
+
 a = 1 # a is the integer
 b = 5.22 # b is the floating point numeber
 c = "Aditya" # c is a string
